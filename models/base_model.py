41c2b1ddd5eed06899b4459390472e2e11cc9c71
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-02-14 18:21:12 -0500

    updated and finished task 5

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Module to create Base model"""
 from datetime import datetime
 from datetime import date
+import models
 import uuid 
 
 
@@ -15,16 +16,18 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == 'id':
                     self.id = str(uuid.uuid1())
-                if key == 'created_at':
+                elif key == 'created_at':
                     self.created_at = datetime.strptime(value, date_format)
-                if key == 'updated_at':
+                elif key == 'updated_at':
                     self.updated_at = datetime.strptime(value, date_format)
-                if key != '__class__':
-                    setattr(self, key, value)
+                else:
+                    if key != '__class__':
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid1())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
     
     def __str__(self):
         """String representation"""
@@ -34,13 +37,15 @@ class BaseModel:
         """updates the public instance attribute updated_at
         with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
         base_dict = self.__dict__.copy()
         base_dict['__class__'] = self.__class__.__name__
-        base_dict['updated_at'] = self.updated_at.isoformat()
         base_dict['created_at'] = self.created_at.isoformat()
+        base_dict['updated_at'] = self.updated_at.isoformat()
+        base_dict['id'] = self.id
         return(base_dict)
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """Module to create dile atorage"""
+import json
+import models
+from models.base_model import BaseModel
+
 
 
 class FileStorage:
@@ -8,13 +12,29 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
+        return self.__objects
     
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
+        if obj is not None:
+            key = obj.__class__.__name__+ '.' + obj.id
+            self.__objects[key] = obj
     
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
+        json_dict = {}
+        for key in self.__objects.keys():
+            json_dict[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as file:
+            file.write(json.dumps(json_dict))
+
     
     def reload(self):
         """deserializes the JSON file to __objects"""
-        
+        try:
+            with open(self.__file_path, 'r') as file:
+                des_dict = json.load(file)
+            for key, value in des_dict.items():
+               self.__objects[key] = eval(value['__class__'])(**value)
+        except:
+            pass
