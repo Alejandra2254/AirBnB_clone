87bcd4780851c2eed702c00c2ea44c98d6afb96f
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-02-15 13:36:10 -0500

    update of task 7 module ceate and show in the console

--- console.py
+++ console.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """ the console """
 import cmd
+import sys
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+classes = {"BaseModel": BaseModel, "User": User}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +25,45 @@ class HBNBCommand(cmd.Cmd):
         """ empty line """
         pass
 
+    def do_create(self, arg):
+        """ Creates a new instance, depending of the class
+        saves it (to the JSON file) and prints the id"""
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg in classes:
+            new_instance = classes[arg]()
+            storage.save()
+            print(new_instance.id)
+        else:
+            print("** class doesn't exist **")
+    
+    def do_show(self, arg):
+        """Prints the string representation of an instance
+        based on the class name and id"""
+        line = parse(arg)
+        if line == []:
+            print("** class name missing **")
+        elif line[0] in classes:
+            if len(line) == 1:
+                print("** instance id missing **")
+            if len(line) == 2:
+                try:
+                    all_objs = storage.all()
+                    key = line[0] + "." + line[1]
+                    obj = all_objs[key]
+                    print(obj)
+                except KeyError:
+                    print("** class doesn't exist **")
+    
+    def do_delete(self, arg):
+        """Deletes an instance based on the class name and id"""
+
+
+def parse(arg):
+    """Convert a series of zero or more numbers to an
+    argument list"""
+    return list(map(str, arg.split()))
+
 
     """ HELPS """
 
@@ -30,7 +74,6 @@ class HBNBCommand(cmd.Cmd):
     def help_EOF(self):
         """ help command """
         print("[USAGE] Ctrl + d")
-
 
 
 if __name__ == '__main__':
--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 from datetime import date
 import models
-import uuid 
+import uuid
 
 
 class BaseModel:
@@ -28,18 +28,18 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
-    
+
     def __str__(self):
         """String representation"""
-        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
-    
+        return("[{}] ({}) {}".format(self.__class__.__name__,
+                                    self.id, self.__dict__))
+
     def save(self):
         """updates the public instance attribute updated_at
         with the current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
 
-    
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
         base_dict = self.__dict__.copy()
@@ -48,4 +48,3 @@ class BaseModel:
         base_dict['updated_at'] = self.updated_at.isoformat()
         base_dict['id'] = self.id
         return(base_dict)
-
