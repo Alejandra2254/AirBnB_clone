e959d500f267aef38f73a483d5808ea04d1de6c8
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-02-16 01:38:45 -0500

    Adding more test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,16 +2,27 @@
 """Base Model Test module"""
 import unittest
 import uuid
+from datetime import datetime  # for strftime
+from datetime import date
 from models import storage
 from models.base_model import BaseModel
 
 class TestBasemodel(unittest.TestCase):
     """Class to test the BaseModel Class"""
     
-    def test_0_leng_id(self):
+    def test_0__id(self):
         model_1 = BaseModel()
+        model_2 = BaseModel()
         self.assertEqual(len(model_1.id), 36)
-    
-    def test_1_type_id(self):
-        model_1 = BaseModel()
         self.assertEqual(type(model_1.id), str)
+        self.assertNotEqual(model_1.id, model_2.id)
+    
+    def test_1_created_at(self):
+        model_1 = BaseModel()
+        self.assertEqual(type(model_1.created_at), datetime)
+        self.assertEqual(model_1.created_at, model_1.updated_at)
+    
+    def test_2_updated_at(self):
+        model_1 = BaseModel()
+        self.assertEqual(type(model_1.updated_at), datetime)
+        self.assertEqual(model_1.created_at, model_1.updated_at)
