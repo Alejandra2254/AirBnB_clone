19d44006d7131bacad8de606e0199c26e0b725cd
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-02-16 01:02:31 -0500

    Update file storage with other classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,11 @@ import json
 import models
 from models.base_model import BaseModel
 from models.user import User
-
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
 
 
 class FileStorage:
@@ -14,13 +18,13 @@ class FileStorage:
     def all(self):
         """returns the dictionary __objects"""
         return self.__objects
-    
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
-            key = obj.__class__.__name__+ '.' + obj.id
+            key = obj.__class__.__name__ + '.' + obj.id
             self.__objects[key] = obj
-    
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_dict = {}
@@ -29,14 +33,12 @@ class FileStorage:
         with open(self.__file_path, 'w') as file:
             file.write(json.dumps(json_dict))
 
-    
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, 'r') as file:
                 des_dict = json.load(file)
             for key, value in des_dict.items():
-               self.__objects[key] = eval(value['__class__'])(**value)
-        except:
+                self.__objects[key] = eval(value['__class__'])(**value)
+        except FileNotFoundError:
             pass
-
