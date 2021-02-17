41afb200afe73b7f66167b5b37ec59cc34ccbf47
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-02-17 10:27:53 -0500

    trying to modify reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,17 +35,11 @@ class FileStorage:
             file.write(json.dumps(json_dict))
 
     def reload(self):
-        """ deserialization """
-        classes = {'BaseModel': BaseModel,
-                   'User': User, 'Place': Place,
-                   'State': State, 'City': City,
-                   'Amenity': Amenity, 'Review': Review}
+        """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as f1:
-                file_store = json.load(f1)
-                for key, value in file_store.items():
-                    if '__class__' in value:
-                        val = classes[value['__class__']](**value)
-                        self.__objects[key] = val
-        except:
+            with open(self.__file_path, 'r') as file:
+                des_dict = json.load(file)
+            for key, value in des_dict.items():
+                self.__objects[key] = eval(value['__class__'])(**value)
+        except FileNotFoundError:
             pass
