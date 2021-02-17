06ced67ae9b03e367e03d9cab01e2d3d662b251d
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-02-16 21:35:57 -0500

    update test for all the classes

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """updating with instance of FileStorage"""
 
-
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""User Model Test module"""
+"""City Model Test module"""
 import unittest
 import uuid
 from time import sleep
@@ -7,7 +7,7 @@ from datetime import datetime  # for strftime
 from datetime import date
 from models import storage
 from models.base_model import BaseModel
-from models.user import User
+from models.city import City
 
 
 class TestBasemodel(unittest.TestCase):
@@ -15,27 +15,27 @@ class TestBasemodel(unittest.TestCase):
 
     def test_0__id(self):
         """using uuid, if the lenght is 36, is atring and unique"""
-        model_1 = User()
-        model_2 = User()
+        model_1 = City()
+        model_2 = City()
         self.assertEqual(len(model_1.id), 36)
         self.assertEqual(type(model_1.id), str)
         self.assertNotEqual(model_1.id, model_2.id)
 
     def test_1_created_at(self):
         """created_at as datetime type and equal updated"""
-        model_1 = User()
+        model_1 = City()
         self.assertEqual(type(model_1.created_at), datetime)
         self.assertEqual(model_1.created_at, model_1.updated_at)
 
     def test_2_updated_at(self):
         """if update_at is datetime type and equal to created_at"""
-        model_1 = User()
+        model_1 = City()
         self.assertEqual(type(model_1.updated_at), datetime)
         self.assertEqual(model_1.created_at, model_1.updated_at)
 
     def test_3_save_method(self):
         """If updated_at works when save() is called"""
-        b1 = User()
+        b1 = City()
         crtime = b1.created_at
         uptime = b1.updated_at
         sleep(0.1)
@@ -46,7 +46,7 @@ class TestBasemodel(unittest.TestCase):
 
     def test_4_str_method(self):
         """is the string representation is wrong"""
-        model_1 = User()
+        model_1 = City()
         model_1.save()
         str_model = "[" + str(model_1.__class__.__name__) + "] (" + \
                     str(model_1.id) + ") " + str(model_1.__dict__)
@@ -54,7 +54,7 @@ class TestBasemodel(unittest.TestCase):
 
     def test_5_dict_method(self):
         """test to_dict methods return"""
-        model_1 = User()
+        model_1 = City()
         model_1.name = "Alejandra"
         model_1.number = 89
         my_model_dict = model_1.to_dict()
@@ -74,6 +74,6 @@ class TestBasemodel(unittest.TestCase):
     def test_6_kwargs(self):
         """If the value is correct created with kwargs"""
         model_id = str(uuid.uuid4())
-        model_1 = User(id=model_id)
+        model_1 = City(id=model_id)
         self.assertTrue(isinstance(model_1, BaseModel))
         self.assertEqual(model_id, model_1.id)
