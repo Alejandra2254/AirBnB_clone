7640415b34935628fb4fc8d943f97c504a8b5d0f
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-02-16 19:37:47 -0500

    update test file storage

--- tests/test_models/tes_engine/test_file_storage.py
+++ tests/test_models/tes_engine/test_file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Base Model Test module"""
 import unittest
+import json
 from models import storage
 from models.base_model import BaseModel
 
@@ -21,6 +22,24 @@ class Testfilestorage(unittest.TestCase):
         all_objects = storage.all()
 
         self.assertNotEqual(all_objects, {})
+        my_key = str(type(my_model).__name__) + "." + str(my_model.id)
+        self.assertTrue(my_key in all_objects.keys())
+    
+    def test_02_serialization(self):
+        '''checks the correct serialization of an object'''
+        my_model = BaseModel()
+        my_model.name = "test_02"
+        my_model.my_number = 2
+        my_model.save()
+
+        my_key = str(type(my_model).__name__) + "." + str(my_model.id)
+
+        json_file = "file.json"  # storage.__file_path
+        with open(json_file, "r", encoding='utf-8') as f:
+            json_dict = json.load(f)
+
+        exp_dict = json_dict[my_key]
+        self.assertEqual(my_model.to_dict(), exp_dict)
 
 
         
